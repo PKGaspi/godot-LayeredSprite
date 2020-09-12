89ac16dff712fefd77914b9cd7e5466b08a7c178
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-12 13:37:40 +0200

    Add some testing calls

--- Game.gd
+++ Game.gd
@@ -1,10 +1,22 @@
 extends Node
 
+onready var layered_sprite := $LayeredSprite
 
 func _ready() -> void:
-	var layers = $LayeredSprite.get_layers()
+	var layers = layered_sprite.get_layers()
+	print(layered_sprite.get_layer_count())
 	for layer in layers:
-		printt(layer, layers[layer])
+		printt(layer, layers[layer], layered_sprite.get_layer_position(layer))
+	
+	layered_sprite.remove_layer("Makeup")
+	print(layered_sprite.get_layer_count())
+	
+	print(layered_sprite.get_layers())
+	
+	layered_sprite.set_animation("dance")
+	layered_sprite.set_layer_animation("Body", "dance")
+	layered_sprite.set_layer_animation("Hat", "dance")
+	layered_sprite.remove_layer("Hat")
 
 
 func _input(event: InputEvent) -> void:
