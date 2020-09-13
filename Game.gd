72e78ef535fe63b39c48b12b1ef0331970562df2
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-13 12:04:47 +0200

    Added closet and group selection

--- Game.gd
+++ Game.gd
@@ -1,6 +1,8 @@
 extends Node
 
 onready var layered_sprite := $LayeredSprite
+onready var closet := $LayeredSpriteCloset
+onready var group_label := $Group
 
 func _ready() -> void:
 	var layers = layered_sprite.get_layers()
@@ -22,6 +24,10 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit_game") or event.is_action_pressed("ui_accept"):
 		exit_game()
+
+
+func _physics_process(delta: float) -> void:
+	group_label.text = closet.current_group
 
 
 func exit_game(exit_code: int = 0) -> void:
