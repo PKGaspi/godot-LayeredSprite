5d573ec936273f57962cd5f34060caf182cbe300
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-13 15:01:16 +0200

    Implemented LayerGroups as a resource

---
+++ LayerGroup.gd
@@ -0,0 +1,7 @@
+class_name LayerGroup
+extends Resource
+
+
+export var name: String
+export var members: Dictionary
+
--- LayeredSpriteCloset.gd
+++ LayeredSpriteCloset.gd
@@ -8,14 +8,10 @@ extends Node
 export var _layered_sprite_path: NodePath
 var _layered_sprite: LayeredSprite
 
-# Key is a group name and value is an array of layer names that
-# belong to that group. If a layer is not specified in any group,
-# a group for itself is created with its own name.
-export var _groups: Dictionary
-# Key is a layer name and value is an array of Texture or SpriteFrames.
-export var _sprites: Dictionary
+# An array of LayerGroups. Each LayerGroup includes its group name,
+# its members and their sprites.
+export(Array, Resource) var _groups: Array
 
-var _group_names: Array
 var group_index: int = 0 setget set_group_index
 var current_group: String
 
@@ -25,21 +21,6 @@ func _ready() -> void:
 	_layered_sprite = get_node(_layered_sprite_path)
 	assert(_layered_sprite is LayeredSprite)
 	
-	
-	# Add all layers not included in a group to _groups.
-	var layers = _layered_sprite.get_layers()
-	for group in _groups:
-		for group_member in _groups[group]:
-			if not layers.has(group_member):
-				push_warning("Layer with name '" + group_member + "' from group '" + group + "'was not found.")
-			else:
-				layers.erase(group_member)
-	
-	for layer in layers:
-		_groups[layer] = layer
-	
-	
-	_group_names = _groups.keys()
 	set_group_index(0)
 
 
@@ -53,7 +34,7 @@ func next_group() -> void:
 
 
 func set_group_index(value: int) -> void:
-	group_index = fposmod(value, len(_group_names))
-	current_group = _group_names[group_index]
+	group_index = fposmod(value, len(_groups))
+	current_group = _groups[group_index].name
 
 
