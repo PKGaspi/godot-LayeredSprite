5e5396cf5befd09d12f889b4f3a2efabcdf67563
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-14 15:55:54 +0200

    Added more info labels

--- Game.gd
+++ Game.gd
@@ -2,7 +2,8 @@ extends Node
 
 onready var layered_sprite := $LayeredSprite
 onready var closet := $LayeredSpriteCloset
-onready var group_label := $Group
+onready var group_label := $MarginContainer/HBoxContainer/Values/Group/GroupValue
+onready var sprite_label := $MarginContainer/HBoxContainer/Values/Sprite/SpriteValue
 
 func _ready() -> void:
 	var layers = layered_sprite.get_layers()
@@ -28,6 +29,7 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	group_label.text = closet.current_group_name
+	sprite_label.text = str(closet.current_group.sprite_index)
 
 
 func exit_game(exit_code: int = 0) -> void:
