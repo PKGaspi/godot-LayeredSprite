ea049fa586b9f51b4df15f3133b89c51e8380e5f
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-14 15:56:05 +0200

    Removed super duper debug test lines

--- Game.gd
+++ Game.gd
@@ -5,21 +5,7 @@ onready var closet := $LayeredSpriteCloset
 onready var group_label := $MarginContainer/HBoxContainer/Values/Group/GroupValue
 onready var sprite_label := $MarginContainer/HBoxContainer/Values/Sprite/SpriteValue
 
-func _ready() -> void:
-	var layers = layered_sprite.get_layers()
-	print(layered_sprite.get_layer_count())
-	for layer in layers:
-		printt(layer, layers[layer], layered_sprite.get_layer_position(layer))
-	
-	layered_sprite.remove_layer("Makeup")
-	print(layered_sprite.get_layer_count())
-	
-	print(layered_sprite.get_layers())
-	
-	layered_sprite.set_animation("dance")
-	layered_sprite.set_layer_animation("Body", "dance")
-	layered_sprite.set_layer_animation("Hat", "dance")
-	layered_sprite.remove_layer("Hat")
+
 
 
 func _input(event: InputEvent) -> void:
