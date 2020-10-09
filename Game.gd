ddd99a2e2526305e4b65781582b791ad981140b6
Juan Gaspar García <gaxdpar@gmail.com>
2020-10-09 13:43:05 +0200

    Added animation selector

--- Game.gd
+++ Game.gd
@@ -4,8 +4,15 @@ onready var layered_sprite := $LayeredSprite
 onready var closet := $LayeredSpriteCloset
 onready var group_label := $MarginContainer/HBoxContainer/Values/Group/GroupValue
 onready var sprite_label := $MarginContainer/HBoxContainer/Values/Sprite/SpriteValue
+onready var animations_option := $MarginContainer/HBoxContainer/Values/Animations
 
 
+func _ready() -> void:
+	var animations = layered_sprite.get_node("Body").frames.get_animation_names()
+	var i = 0
+	for animation in animations:
+		animations_option.add_item(animation, i)
+		i += 1
 
 
 func _input(event: InputEvent) -> void:
@@ -20,3 +27,7 @@ func _physics_process(delta: float) -> void:
 
 func exit_game(exit_code: int = 0) -> void:
 	get_tree().quit(exit_code)
+
+
+func _on_Animations_item_selected(index: int) -> void:
+	layered_sprite.set_animation(animations_option.get_item_text(index))
